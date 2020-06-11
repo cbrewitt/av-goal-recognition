1360a02d8706a867e3dbb7cf339f1c37de3029c9
Cillian Brewitt <cillianbrewitt@gmail.com>
2020-06-11 17:04:02 +0100

    add folders for image/data

---
+++ av_goal_recognition/base.py
@@ -0,0 +1,17 @@
+import pathlib
+
+
+def get_base_dir():
+    return pathlib.Path(__file__).parent.parent.absolute()
+
+
+def get_data_dir():
+    return get_base_dir() + '/data/'
+
+
+def get_img_dir():
+    return get_base_dir() + '/images/'
+
+
+def get_scenario_config_dir():
+    return get_base_dir() + '/scenario_config/'
