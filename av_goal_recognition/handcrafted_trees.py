5a16b0af996e374682d4e4d25937c21ea83e4171
Cillian Brewitt <cillianbrewitt@gmail.com>
2020-08-11 14:53:41 +0100

    add new features to goal trees

--- av_goal_recognition/handcrafted_trees.py
+++ av_goal_recognition/handcrafted_trees.py
@@ -1,32 +1,28 @@
 import numpy as np
 from av_goal_recognition.decision_tree import Node, ThresholdDecision, BinaryDecision
 
-
-"""
-al features
-path_to_goal_length
-in_correct_lane
-speed
-acceleration
-angle_in_lane
-angle_to_goal
-"""
-
 scenario_trees = {'heckstrasse':
                  {0: Node(0.5, ThresholdDecision(3 / 8 * np.pi, 'angle_to_goal',
-                          Node(0.5, ThresholdDecision(34.5, 'path_to_goal_length',
-                               Node(0.5),
+                          Node(0.5, ThresholdDecision(2, 'speed',
                                Node(0.5, ThresholdDecision(np.pi/18, 'angle_in_lane',
                                     Node(0.7),
                                     Node(0.4, ThresholdDecision(-np.pi/18, 'angle_in_lane',
                                          Node(0.5),
                                          Node(0.3)
                                          ))
+                                    )),
+                               Node(0.4, ThresholdDecision(30, 'oncoming_vehicle_dist',
+                                    Node(0.3),
+                                    Node(0.7)
                                     ))
                                )),
                           Node(0.5, ThresholdDecision(10, 'speed',
                                Node(0.8),
-                               Node(0.2)
+                               Node(0.3, ThresholdDecision(10, 'vehicle_in_front_speed',
+                                    Node(0.8),
+                                    Node(0.5, ThresholdDecision(30, 'vehicle_in_front_dist',
+                                         Node(0.8), Node(0.2)))
+                                    ))
                                ))
                           )),
 
@@ -36,14 +32,17 @@ scenario_trees = {'heckstrasse':
                                     Node(0.8),
                                     Node(0.2)
                                     )),
-                               Node(0.5, ThresholdDecision(56, 'path_to_goal_length',
-                                    Node(0.5),
-                                    Node(0.5, ThresholdDecision(np.pi / 18, 'angle_in_lane',
+                               Node(0.5, ThresholdDecision(2, 'speed',
+                                    Node(0.5, ThresholdDecision(np.pi/18, 'angle_in_lane',
                                          Node(0.7),
-                                         Node(0.4, ThresholdDecision(-np.pi / 18, 'angle_in_lane',
+                                         Node(0.4, ThresholdDecision(-np.pi/18, 'angle_in_lane',
                                               Node(0.5),
                                               Node(0.3)
                                               ))
+                                         )),
+                                    Node(0.4, ThresholdDecision(30, 'oncoming_vehicle_dist',
+                                         Node(0.3),
+                                         Node(0.7)
                                          ))
                                     ))
                                )),
@@ -54,16 +53,19 @@ scenario_trees = {'heckstrasse':
                           )),
 
                   2: Node(0.5, ThresholdDecision(0, 'angle_to_goal',
-                          Node(0.5, ThresholdDecision(30, 'path_to_goal_length',
-                               Node(0.5),
-                               Node(0.5, ThresholdDecision(10, 'speed',
-                                    Node(0.2),
-                                    Node(0.8)
+                          Node(0.5, ThresholdDecision(10, 'speed',
+                               Node(0.2),
+                               Node(0.8, ThresholdDecision(10, 'vehicle_in_front_speed',
+                                    Node(0.8),
+                                    Node(0.5, ThresholdDecision(30, 'vehicle_in_front_dist',
+                                         Node(0.8),
+                                         Node(0.5)
+                                         ))
                                     ))
                                )),
                           Node(0.5, BinaryDecision('in_correct_lane',
-                                           Node(0.9),
-                                           Node(0.1)
+                               Node(0.9),
+                               Node(0.1)
                                ))
                           ))
                  }
--- av_goal_recognition/plot_trees.py
+++ av_goal_recognition/plot_trees.py
@@ -1,6 +1,7 @@
 import pydot
 
 from av_goal_recognition.handcrafted_trees import scenario_trees
+from av_goal_recognition.base import get_img_dir
 
 
 def build_pydot_tree(graph, root, idx='R'):
@@ -19,4 +20,4 @@ scenario_name = 'heckstrasse'
 for goal_idx, root in scenario_trees[scenario_name].items():
     graph = pydot.Dot(graph_type='digraph')
     build_pydot_tree(graph, root)
-    graph.write_png('handcrafted_tree_{}_G{}.png'.format(scenario_name, goal_idx))
+    graph.write_png(get_img_dir() + 'handcrafted_tree_{}_G{}.png'.format(scenario_name, goal_idx))
