900fa5e2c01039a7e1d69dec46e967b29700e8ab
Cillian <cillianbrewitt@gmail.com>
2020-09-16 18:15:36 +0100

    add package requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name="av-goal-recogntion",
       author="Cillian Brewitt",
       version='1.0.0',
-      packages=['av_goal_recognition'])
+      packages=['av_goal_recognition'],
+      install_requires=requirements)
